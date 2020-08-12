ef68c9a78ec912656bcf158395cfe19ff4148a73
unknown <tusha170101073@iitg.ac.in>
2020-08-13 02:54:41 +0530

    Can check if track is playing/paused

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,40 @@
 */
 
 SPOTIFY_URL = "https://open.spotify.com/*";
+
+selectors = {
+    albumArt: ".cover-art img.cover-art-image",
+    play:
+    "#main .Root__now-playing-bar .now-playing-bar__center .player-controls__buttons button.control-button--circled",
+}
 var spotify_tabs = [];
 
-function setSpotifyTabs(){
+function playPause(){
+    
+    chrome.tabs.executeScript(
+        spotify_tabs[0].id,
+        {
+            code: `document.querySelector('${selectors.play}').className`,
+        },
+        (result)=>{
+            /*if(result.length==0) document.getElementById("Present").innerHTML = "Playing";
+            else document.getElementById("Present").innerHTML = "Paused";*/
+            if(result[0].includes("pause") ) document.getElementById("Present").innerHTML = "Playing";
+            else document.getElementById("Present").innerHTML = "Paused";
+            //document.getElementById("Present").innerHTML = result[0];
+        }
+    );
+}
+function setSpotifyTabs(callback){
     chrome.tabs.query({url: SPOTIFY_URL},(tabs)=>{
         spotify_tabs = tabs;
         document.getElementById("Present").innerHTML = spotify_tabs.length;
+        callback();
     });
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    setSpotifyTabs();
+    setSpotifyTabs(function(){
+        document.getElementById("Check").addEventListener("click",playPause);
+    });
   });
